c82b68635e6ac4121d15f928fcf45a0150fed037
vaibhav Pandey <vaibhav.pandey0798@gmail.com>
2024-02-07 23:01:37 +0530

    updatedImageURL

--- Frontend/indic-lan/src/Components/Hero.jsx
+++ Frontend/indic-lan/src/Components/Hero.jsx
@@ -4,6 +4,7 @@ import { MdKeyboardVoice } from "react-icons/md";
 import { FaImage } from "react-icons/fa";
 import { ImCancelCircle } from "react-icons/im";
 import { FaRegFileAudio } from "react-icons/fa";
+import { FaRegFileImage } from "react-icons/fa";
 
 
 const Hero = () => {
@@ -34,10 +35,42 @@ const Hero = () => {
     const [tolan, setTolan] = useState("");
     const [textField, setTextField] = useState("");
     const [voiceField, setVoiceField] = useState("");
-    const [imgField, setImgField] = useState("");
+    const [pic, setPic] = useState();
     const [typeText, setTypeText] = useState(false);
     const [typeVoice, setTypeVoice] = useState(false);
     const [typeImg, setTypeImg] = useState(false);
+
+    const postImage = (pics) => {
+        if (pics === undefined) {
+            toast.warn("Please select an Image");
+            return;
+        }
+        if (pics.type === "image/jpeg" || pics.type === "image/png" || pics.type === "image/jpg") {
+            const data = new FormData();
+            data.append("file", pics);
+            data.append("upload_preset", "Chatmi-App");
+            data.append("cloud_name", "vaibhavpandey0");
+            fetch("https://api.cloudinary.com/v1_1/vaibhavpandey0/image/upload", {
+                method: "post",
+                body: data
+            }).then((res) => res.json()).then((data) => {
+                setPic(data.url.toString());
+                console.log(data.url.toString());
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+        else {
+            toast.warn("Please select an Image!");
+        }
+    }
+
+    const postVoice = (recs) => {
+        if (recs === undefined) {
+            toast.warn("Please select an Image");
+            return;
+        }
+    }
 
     return (
         <div className="w-full h-[51.8rem] flex items-center justify-center border">
@@ -112,7 +145,7 @@ const Hero = () => {
                                             <div className="mr-5 cursor-pointer text-2xl" onClick={() => setTypeVoice(false)}><ImCancelCircle /></div>
                                         </div>
                                         <div className="w-full h-[40rem] flex items-center justify-center overflow-auto">
-                                            <label className="w-3/5 h-2/4 flex flex-col justify-center items-center gap-3 rounded-lg font-semibold text-xl text-white bg-indigo-500 border"><div>Select audio file</div><div><FaRegFileAudio /></div><input type="file" accept=".mp3,audio/*" className=" hidden" /></label>
+                                            <label className="w-3/5 h-2/4 flex flex-col justify-center items-center gap-3 rounded-lg font-semibold text-xl text-white bg-indigo-500 border"><div>Select audio file</div><div><FaRegFileAudio /></div><input type="file" accept=".mp3,audio/*" onChange={(e) => postVoice(e.target.files[0])} className=" hidden" /></label>
                                         </div>
                                         <div className="w-full h-[4rem] flex justify-end items-center p-5">
                                             <div className="bg-indigo-500 text-white font-semibold px-7 py-3 rounded-lg border">Next</div> {/* Put what to do, backend on this button */}
@@ -121,7 +154,26 @@ const Hero = () => {
                                 </div>
                             )}
 
-                            <div className="w-52 h-full flex flex-col justify-center items-center cursor-pointer text-xl hover:bg-indigo-500 hover:text-white duration-300 rounded-xl border"><FaImage />Image</div>
+                            <div className="w-52 h-full flex flex-col justify-center items-center cursor-pointer text-xl hover:bg-indigo-500 hover:text-white duration-300 rounded-xl border" onClick={() => setTypeImg(true)}><FaImage />Image</div>
+
+                            {typeImg && (
+                                <div className="w-full h-screen absolute top-0 left-0 backdrop-blur-sm bg-white/50 flex justify-center items-center">
+                                    <div className="w-2/5 h-[40rem] rounded-xl flex flex-col justify-center items-center p-8 gap-3 bg-white border">
+                                        <div className="w-full h-[4rem] flex items-center justify-between p-3">
+                                            <div className="ml-5">
+                                                <p className="h-1/4 font-semibold text-xl border-b-4 border-black">Input</p>
+                                            </div>
+                                            <div className="mr-5 cursor-pointer text-2xl" onClick={() => setTypeImg(false)}><ImCancelCircle /></div>
+                                        </div>
+                                        <div className="w-full h-[40rem] flex items-center justify-center overflow-auto">
+                                            <label className="w-3/5 h-2/4 flex flex-col justify-center items-center gap-3 rounded-lg font-semibold text-xl text-white bg-indigo-500 border"><div>Select Image file</div><div><FaRegFileImage /></div><input type="file" accept="image/png, image/jpeg, image/jpg" onChange={(e) => postImage(e.target.files[0])} className=" hidden" /></label>
+                                        </div>
+                                        <div className="w-full h-[4rem] flex justify-end items-center p-5">
+                                            <div className="bg-indigo-500 text-white font-semibold px-7 py-3 rounded-lg border">Next</div> {/* Put what to do, backend on this button */}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
 
                         </div>
                     </div>
